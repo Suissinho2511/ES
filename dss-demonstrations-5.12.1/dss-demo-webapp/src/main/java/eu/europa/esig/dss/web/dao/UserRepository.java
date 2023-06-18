a06c99c19649f6a84a2113dc249346991e93be90
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2023-06-18 18:02:23 +0100

    Delete dss-demonstrations-5.12.1 directory

--- dss-demonstrations-5.12.1/dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.europa.esig.dss.web.dao;
-
-import eu.europa.esig.dss.web.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
--- dss-demonstrations-5.12.1/dss-standalone-app/src/main/java/eu/europa/esig/dss/standalone/enumeration/SignatureOption.java
+++
@@ -1,9 +0,0 @@
-package eu.europa.esig.dss.standalone.enumeration;
-
-public enum SignatureOption {
-
-    TL_SIGNING,
-
-    XML_MANIFEST_SIGNING;
-
-}
